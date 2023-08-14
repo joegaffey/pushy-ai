a90bd240e7894c0bca63284b342bd109911ec31d
Glitch (pushy-ai-dev) <none>
2023-08-14 09:28:26 +0000

    Turk driver connection management

--- TurkAI.js
+++ TurkAI.js
@@ -1,4 +1,4 @@
-export default class Turk {
+export default class TurkAI {
   
   constructor() {
     this.actions = [];
--- TurkServer.js
+++ TurkServer.js
@@ -3,6 +3,8 @@ import { WebSocketServer } from 'ws';
 const turkWsServer = new WebSocketServer({ noServer: true });
 export default turkWsServer;
 
+const unassigned = [];
+
 function broadcast(server, data) {
   server.clients.forEach(client => {
     if (client.readyState === 1) {
@@ -11,17 +13,23 @@ function broadcast(server, data) {
   });
 }
 
+turkWsServer.getDriver = () => {
+  return unassigned.pop(); 
+}
+
 turkWsServer.on("connection", ws => {
   ws.id = Date.now();
+  unassigned.push(ws);
   console.log('Turk' + turkWsServer.clients.size +' connected:', ws.id);
+  
   const message = { cid: ws.id, message: 'Connected!' };
   broadcast(turkWsServer, message);
   
   ws.on("message", data => {
-    console.log("Message received from connection Id:", ws.id);
+    console.log("Message from connection Id:", ws.id);
     const message = JSON.parse(data);
     console.log(message);
-    message.cid = ws.id; 
+    message.cid = ws.id;
     broadcast(turkWsServer, message);
   });
 
